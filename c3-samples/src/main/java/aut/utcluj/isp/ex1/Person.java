7f2d74a325b42d6c9d587f9b7afe4ccfcc503254
Banculea Elena-Andreea <@andreeabanculea01@gmail.com>
2022-05-06 13:13:20 +0300

    Proiect_Lab_11

---
+++ c3-samples/src/main/java/aut/utcluj/isp/ex1/Person.java
@@ -0,0 +1,27 @@
+package aut.utcluj.isp.ex1;
+
+/**
+ * @author stefan
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName) {
+        this.lastName = "";
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
